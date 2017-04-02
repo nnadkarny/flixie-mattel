bb4fbe6dd8f0c9950f7a596f009e32043d438cee
Nikhil <nnadkarny@gmail.com>
2017-04-01 20:50:31 -0400

    4.6.32 Sections Drag & Drop - re-added if-else

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -25,15 +25,12 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
-  end
-  # def current_course
-  #   if params[:course_id]
-  #     @current_course ||= Course.find(params[:course_id])
-  #   else
-  #     current_section.course
-  #   end
-  # end  
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
+  end  
 
   def require_authorized_for_current_course
     if current_course.user != current_user
